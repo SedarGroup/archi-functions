2af0e66ce9d9291170ce13914e5140b7f87a7c1b
Ibrahima <ibracool99@gmail.com>
2022-03-14 22:32:31 +0100

    init working

---
+++ netlify/functions/index.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+exports.handler = (event, context, callback) => {
+  const mail = event.queryStringParameters.email;
+  var con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+  });
+  con.connect(function (err) {
+    if (err) {
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify(err)
+      })
+    }
+    console.log("Connected!");
+    var sql = `INSERT INTO emails (email) VALUES ('${mail}')`;
+    con.query(sql, function (err, result) {
+      if (err) {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify(err)
+        })
+      }
+      callback(null, {
+        statusCode: 200,
+        body: '1 mail inserted'
+      })
+    });
+  });
+}
